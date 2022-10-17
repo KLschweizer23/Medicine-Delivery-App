371da5f9421b28ae3c68d0cb78d5f8346f8f102b
kl <schweizer23.ss@gmail.com>
2022-10-17 23:37:56 +0800

    Added Web Security

---
+++ src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/securities/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.medicinedeliveryapp.medicinedeliveryapp.securities;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.medicinedeliveryapp.medicinedeliveryapp.details.service.CustomUserDetailsService;
+
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+	}
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+        .antMatchers("/dashboard").authenticated()
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+            .loginPage("/login")
+            .usernameParameter("email")
+            .failureForwardUrl("")
+            .defaultSuccessUrl("/dashboard")
+            .permitAll()
+        .and().logout().logoutSuccessUrl("/").permitAll();
+
+        return http.build();
+    }
+    
+}
