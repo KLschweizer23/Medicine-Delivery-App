3b9ef3ef0ab1e4aeedb5c48bcba8ea1a26b56a1c
kl <schweizer23.ss@gmail.com>
2022-10-22 17:35:02 +0800

    Register role in controller and pass data successful

--- src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/AppController.java
+++ src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/AppController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.medicinedeliveryapp.medicinedeliveryapp.objects.Buyer;
+import com.medicinedeliveryapp.medicinedeliveryapp.objects.Doctor;
+import com.medicinedeliveryapp.medicinedeliveryapp.objects.Driver;
+import com.medicinedeliveryapp.medicinedeliveryapp.objects.Pharmacist;
 import com.medicinedeliveryapp.medicinedeliveryapp.objects.User;
 
 @RestController
@@ -41,7 +45,7 @@ public class AppController {
     }
 
     @PostMapping("/process-user")
-    public ModelAndView processUserRegistration(User user, Model model){
+    public ModelAndView processUserRegistration(User user){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("register_role.html");
 
@@ -49,7 +53,13 @@ public class AppController {
         String encodedPassword = encoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
 
-        model.addAttribute("user", user);
+        mav.addObject("user", user);
+        mav.addObject("buyer", new Buyer());
+        mav.addObject("doctor", new Doctor());
+        mav.addObject("pharmacist", new Pharmacist());
+        mav.addObject("driver", new Driver());
+
+        mav.addObject("role", user.getRole());
 
         return mav;
     }
