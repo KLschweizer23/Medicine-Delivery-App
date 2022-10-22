b5a0af56cbcf4cd0df5391ef0dca8229eb9f3ab5
kl <schweizer23.ss@gmail.com>
2022-10-22 17:02:17 +0800

    Added process user to redirect to the role

--- src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/AppController.java
+++ src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/AppController.java
@@ -1,7 +1,9 @@
 package com.medicinedeliveryapp.medicinedeliveryapp;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,6 +36,20 @@ public class AppController {
         mav.setViewName("register.html");
 
         model.addAttribute("user", new User());
+
+        return mav;
+    }
+
+    @PostMapping("/process-user")
+    public ModelAndView processUserRegistration(User user, Model model){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("register_user_role.html");
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        model.addAttribute("user", user);
 
         return mav;
     }
