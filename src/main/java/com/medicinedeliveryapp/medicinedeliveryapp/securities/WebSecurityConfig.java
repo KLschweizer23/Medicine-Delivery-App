56233bcb02993f71c11bbda5d3fd1e7ed546658b
kl <schweizer23.ss@gmail.com>
2022-11-02 17:10:10 +0800

    added my-order to the websecurity

--- src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/securities/WebSecurityConfig.java
+++ src/main/java/com/medicinedeliveryapp/medicinedeliveryapp/securities/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-        .antMatchers("/dashboard", "/cart", "/checkout").authenticated()
+        .antMatchers("/dashboard", "/cart", "/checkout", "/my-order").authenticated()
         .anyRequest().permitAll()
         .and()
         .formLogin()
